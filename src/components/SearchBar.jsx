87396a0ecadf70b1fbbd34e10f649e2ea7122e80
SonDangJH <son.dang@journeyh.io>
2024-08-19 10:44:57 +0700

    fix: show error when note content is blank

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 // import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { IoFilterOutline } from "react-icons/io5";
 import { RxMagnifyingGlass } from "react-icons/rx";
-export default function SearchBar(props) {
+export default function SearchBar({setSearchKey, sortNote}) {
     return (
         <div className="flex flex-row gap-3 justify-center  items-center w-full px-3">
             <div className="relative w-full">
@@ -11,7 +11,7 @@ export default function SearchBar(props) {
                   </div>
                 <input type="text" id="input-group-1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search to do here ..."
                     onChange={(e)=>{
-                        props.setSearchKey(e.target.value);
+                        setSearchKey(e.target.value);
                     }}/>
             </div>
             <Menu as="div" className="relative inline-block text-left">
@@ -23,12 +23,12 @@ export default function SearchBar(props) {
                 <MenuItems transition className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in">
                     <div className="py-1">
                         <MenuItem>
-                            <a className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900" onClick={()=>{props.sortNote("ASC")}}>
+                            <a className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900" onClick={()=>{sortNote("ASC")}}>
                             Sort by time ASC
                             </a>
                         </MenuItem>
                         <MenuItem>
-                            <a className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900" onClick={()=> {props.sortNote("DESC")}}>
+                            <a className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900" onClick={()=> {sortNote("DESC")}}>
                             Sort by time DESC
                             </a>
                         </MenuItem>
--- src/containers/ToDoListPage.jsx
+++ src/containers/ToDoListPage.jsx
@@ -1,88 +1,102 @@
-import { useEffect, useState } from "react"
-import EditNote from "../components/EditNote"
-import SearchBar from "../components/SearchBar"
-import ToDoItem from "../components/ToDoItem"
+import { useEffect, useState } from "react";
+import EditNote from "../components/EditNote";
+import SearchBar from "../components/SearchBar";
+import ToDoItem from "../components/ToDoItem";
+import toast, { Toaster } from 'react-hot-toast';
 
 export default function ToDoListPage() {
-    const [mode, setMode] = useState("Add");
-    const [searchKey, setSearchKey] = useState("");
-    const [notes, setNotes] = useState([]); 
-    const [backUpNotes, setBackUpNotes] = useState([]);
-    const [currentNote, setCurrentNote] = useState();
-    const [noteBoardContent, setNoteBoardContent] = useState("");
+  const [mode, setMode] = useState("Add");
+  const [searchKey, setSearchKey] = useState("");
+  const [notes, setNotes] = useState([]);
+  const [backUpNotes, setBackUpNotes] = useState([]);
+  const [currentNote, setCurrentNote] = useState();
+  const [noteBoardContent, setNoteBoardContent] = useState("");
 
-    function actionNote() {
-        if (mode === "Add") {
-            setNotes(prev => [...prev, {
-                id: new Date().toTimeString(),
+  function actionNote() {
+    if (noteBoardContent === "") {
+      toast.error("Note can not be empty, please type some letter!")
+      return;
+    }
+    else {
+      if (mode === "Add") {
+        setNotes((prev) => [
+          ...prev,
+          {
+            id: new Date().toTimeString(),
+            content: noteBoardContent,
+            date: new Date(),
+          },
+        ]);
+      } else {
+        if (!currentNote.id) return;
+        const newNoteList = notes.map((note) =>
+          note.id !== currentNote.id
+            ? note
+            : {
+                ...note,
                 content: noteBoardContent,
-                date: new Date()
-            }]);
-        }
-        else {
-            if (!currentNote.id) return;
-            const newNoteList = notes.map(note => note.id !== currentNote.id ? note : {
-                ... note, content: noteBoardContent, date: new Date()
-            });
-            setNotes(newNoteList);
-        }
-        setNoteBoardContent("");
-        setCurrentNote();
-    }
-
-    function deleteNote(noteId) {
-        const newNoteList = notes.filter(note => note.id !== noteId);
+                date: new Date(),
+              }
+        );
         setNotes(newNoteList);
+      }
+      setNoteBoardContent("");
+      setCurrentNote();
+      setMode("Add");
     }
+  }
 
-    function searchNote(searchWord) {
-        const newNoteList = notes.filter(note => note.content.includes(searchWord));
-        setBackUpNotes(newNoteList);
-    }
+  function deleteNote(noteId) {
+    const newNoteList = notes.filter((note) => note.id !== noteId);
+    setNotes(newNoteList);
+  }
 
-    function sortNote(type) {
-        if (type === "ASC") {
-            backUpNotes.sort((a, b) => a.date.getTime() - b.date.getTime());
-        }
-        else {
-            backUpNotes.sort((a, b) => b.date.getTime() - a.date.getTime());
-        }
-        setBackUpNotes(backUpNotes.filter(item=>item));
-    }
+  function searchNote(searchWord) {
+    const newNoteList = notes.filter((note) =>
+      note.content.includes(searchWord)
+    );
+    setBackUpNotes(newNoteList);
+  }
 
-    useEffect(()=>{
-        searchNote(searchKey);
-    }, [searchKey]);
+  function sortNote(type) {
+    const sortFn = type === 'ASC' ? (a, b) => a.date.getTime() - b.date.getTime() : (a, b) => b.date.getTime() - a.date.getTime();
+    setBackUpNotes(prev => [...prev.sort(sortFn)]);
+  }
 
-    useEffect(()=>{
-        setBackUpNotes(notes);
-    }, [notes])
+  useEffect(() => {
+    searchNote(searchKey);
+  }, [searchKey]);
 
-    return (
-        <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-6 ">
-            <div className="flex flex-col gap-6">
-                <SearchBar 
-                    setSearchKey={setSearchKey} 
-                    sortNote={sortNote}/>
-                <div className="overflow-y-scroll h-[500px] flex flex-col gap-3 px-3">
-                    {backUpNotes.map(noteDetail => 
-                        <ToDoItem 
-                            note={noteDetail} 
-                            key={noteDetail.id} 
-                            setMode={setMode} 
-                            setCurrentNote={setCurrentNote}
-                            setNoteBoardContent={setNoteBoardContent}
-                            deleteNote={deleteNote}/>
-                    )}
-                </div>
-            </div>
-            <EditNote 
-                mode={mode} 
-                actionNote={actionNote} 
-                setMode={setMode}
-                currentNote={currentNote}
-                noteBoardContent={noteBoardContent}
-                setNoteBoardContent={setNoteBoardContent}/>
+  useEffect(() => {
+    setBackUpNotes(notes);
+  }, [notes]);
+
+  return (
+    <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-6 ">
+      <Toaster/>
+      <div className="flex flex-col gap-6">
+        <SearchBar setSearchKey={setSearchKey} sortNote={sortNote} />
+        <div className="overflow-y-scroll h-[500px] flex flex-col gap-3 px-3">
+          {backUpNotes.map((noteDetail) => (
+            <ToDoItem
+              note={noteDetail}
+              key={noteDetail.id}
+              setMode={setMode}
+              setCurrentNote={setCurrentNote}
+              setNoteBoardContent={setNoteBoardContent}
+              deleteNote={deleteNote}
+            />
+          ))}
         </div>
-    )
+      </div>
+      <EditNote
+        mode={mode}
+        actionNote={actionNote}
+        setMode={setMode}
+        currentNote={currentNote}
+        noteBoardContent={noteBoardContent}
+        setNoteBoardContent={setNoteBoardContent}
+      />
+    </div>
+  );
 }
